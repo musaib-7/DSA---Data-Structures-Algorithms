2fdcb2916a21f4b98cf8746c47fb3810a180bce3
BinaryMusaib <musaib_bilal@live.com>
2023-03-26 13:11:09 +0530

    Update DoublyLinkedList

--- DSAModule/DoublyLinkedList.java
+++ DSAModule/DoublyLinkedList.java
@@ -1,3 +1,95 @@
 public class DoublyLinkedList {
-    
+
+    private ListNode head;
+    private ListNode tail;
+    private int length;
+
+    public DoublyLinkedList() {
+
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    private class ListNode {
+
+        private int value;
+        private ListNode previous;
+        private ListNode next;
+
+        public ListNode(int value) {
+            this.value = value;
+        }
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void insertAtLast(int value) {
+
+        ListNode newNode = new ListNode(value);
+
+        if(isEmpty()) {
+            head = newNode;
+        } else {
+            tail.next = newNode;
+        }
+        newNode.previous = tail;
+        tail = newNode;
+        length++;
+    }
+
+    public void insertAtBeginning(int value) {
+
+        ListNode newNode = new ListNode(value);
+
+        if(isEmpty()) {
+            tail = newNode;
+        } else {
+            head.previous = newNode;
+        }
+        newNode.next = head;
+        head = newNode;
+        length++;
+    }
+
+    public void displayForward() {
+
+        ListNode temp = head;
+        
+        while(temp != null) {
+            System.out.print(temp.value + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public void displayBackward() {
+
+        ListNode temp = tail;
+
+        while(temp != null) {
+            System.out.print(temp.value + " -> ");
+            temp = temp.previous;
+        }
+        System.out.println("null");
+    }
+    public static void main(String[] args) {
+        
+        DoublyLinkedList dll = new DoublyLinkedList();
+        dll.insertAtLast(1);
+        dll.insertAtLast(2);
+        dll.insertAtLast(3);
+        dll.insertAtBeginning(0);
+        dll.insertAtBeginning(-1);
+
+        dll.displayForward();
+        System.out.println("---------------------------");
+        dll.displayBackward();
+    }
 }
